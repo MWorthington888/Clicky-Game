dbf6d81457143dc5e56d5b33e2000bdb941d8fc9
Mike Worthington <mworthington888@gmail.com>
2019-03-09 23:50:12 -0600

    Updates

---
+++ precache-manifest.2945e68ca65919841b06b563cf840b42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61a0df336c60dbe48a52",
+    "url": "/Clicky-Game/static/js/runtime~main.43da1946.js"
+  },
+  {
+    "revision": "5ece402640f337f09992",
+    "url": "/Clicky-Game/static/js/main.1e4672ac.chunk.js"
+  },
+  {
+    "revision": "ff661de6195320cfc0eb",
+    "url": "/Clicky-Game/static/js/2.822769df.chunk.js"
+  },
+  {
+    "revision": "5ece402640f337f09992",
+    "url": "/Clicky-Game/static/css/main.34fc0717.chunk.css"
+  },
+  {
+    "revision": "25c3a157cc122d56853857374fafe08c",
+    "url": "/Clicky-Game/index.html"
+  }
+];
